f67be34536530e6367093a6c1659f82f69e94288
Andres Alejandro Guaman Taco <andres.guaman.taco@udla.edu.ec>
2024-04-17 00:26:19 -0500

    Correcion codigo script

--- Assets/Scripts/ChangeColor.cs
+++ Assets/Scripts/ChangeColor.cs
@@ -1,39 +1,3 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.UI;
-
-//public class ChangeColor : MonoBehaviour
-//{
-//    [SerializeField] private GameObject sphere;
-//    private Renderer sphereRenderer;
-//    private Color newSphereColor;
-//    private float randomChannelOne, randomChannelTwo, randomChannelThree;
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        sphereRenderer = sphere.GetComponent<Renderer>();
-//        gameObject.GetComponent<Button>().onClick.AddListener(ChangeSpereColor);
-//    }
-
-//    // Update is called once per frame
-//    void Update()
-//    {
-
-//    }
-
-//    private void ChangeSpereColor()
-//    {
-//        randomChannelOne = Random.Range(0f, 1f);
-//        randomChannelTwo = Random.Range(0f, 1f);
-//        randomChannelThree = Random.Range(0f, 1f);
-
-//        newSphereColor = new Color(randomChannelOne, randomChannelTwo, randomChannelThree, 1f);
-
-//        sphereRenderer.material.SetColor("_Color", newSphereColor);
-//    }
-//}
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -51,8 +15,7 @@ public class ChangeColor : MonoBehaviour
     void Start()
     {
         sphereRenderer = sphere.GetComponent<Renderer>();
-        // Asignar la tecla C para cambiar de color
-        // Puedes cambiar la tecla a la que desees
+        
         if (targetObject != null)
         {
             // Cambiar el color inicial del objeto target al color de la esfera
@@ -63,7 +26,7 @@ public class ChangeColor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Detectar cuando se presiona la tecla C
+        
         if (Input.GetKeyDown(KeyCode.C))
         {
             ChangeSphereColor(); // Cambiar el color de la esfera
